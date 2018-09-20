5c065c0ed318b4aeccaa4d107ed726d4808a8483
Kiyoshi Watanabe <kiwatana@hotmail.co.jp>
2018-09-19 23:25:59 -0700

    replaing to a correct content

--- linux/boot.asm
+++ linux/boot.asm
@@ -1,153 +1,302 @@
-<!DOCTYPE html>
-<!-- saved from url=(0094)https://web.archive.org/web/20050428001304/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Wayback Machine</title>
-<script src="./boot_files/analytics.js" type="text/javascript"></script>
-<script type="text/javascript">window.addEventListener('DOMContentLoaded',function(){var v=archive_analytics.values;v.service='wb';v.server_name='wwwb-app100.us.archive.org';v.server_ms=294;archive_analytics.send_pageview({});});</script><link rel="stylesheet" type="text/css" href="./boot_files/banner-styles.css">
-<link rel="stylesheet" type="text/css" href="./boot_files/iconochive.css">
-<script src="./boot_files/jquery-1.11.1.min.js"></script>
-</head>
-<body style="overflow:hidden;"><div id="wm-ipp" lang="en" style="display: block; direction: ltr;">
-<div style="position:fixed;left:0;top:0;right:0;">
-<div id="wm-ipp-inside">
-  <div style="position:relative;">
-    <div id="wm-logo" style="float:left;width:130px;padding-top:10px;">
-      <a href="https://web.archive.org/web/" title="Wayback Machine home page"><img src="./boot_files/wayback-toolbar-logo.png" alt="Wayback Machine" width="110" height="39" border="0"></a>
-    </div>
-    <div class="r" style="float:right;">
-      <div id="wm-btns" style="text-align:right;height:25px;">
-                  <div id="wm-save-snapshot-success">success</div>
-          <div id="wm-save-snapshot-fail">fail</div>
-          <a href="https://web.archive.org/web/20050428001304/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm#" onclick="__wm.saveSnapshot(&#39;http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm&#39;, &#39;20050428001304&#39;)" title="Share via My Web Archive" id="wm-save-snapshot-open" style="display: none;">
-            <span class="iconochive-web"></span>
-          </a>
-          <a href="https://archive.org/account/login.php" title="Sign In" id="wm-sign-in" style="display: inline-block;">
-            <span class="iconochive-person"></span>
-          </a>
-          <span id="wm-save-snapshot-in-progress" class="iconochive-web" style="display: none;"></span>
-        	<a href="http://faq.web.archive.org/" title="Get some help using the Wayback Machine" style="top:-6px;"><span class="iconochive-question" style="color:rgb(87,186,244);font-size:160%;"></span></a>
-	<a id="wm-tb-close" href="https://web.archive.org/web/20050428001304/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm#close" onclick="__wm.h(event);return false;" style="top:-2px;" title="Close the toolbar"><span class="iconochive-remove-circle" style="color:#888888;font-size:240%;"></span></a>
-      </div>
-      <div id="wm-share" style="text-align:right;">
-	<a href="https://web.archive.org/web/20050428001304/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm#" onclick="window.open(&#39;https://www.facebook.com/sharer/sharer.php?u=https://web.archive.org/web/20050428001304/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm&#39;, &#39;&#39;, &#39;height=400,width=600&#39;); return false;" title="Share on Facebook" style="margin-right:5px;" target="_blank"><span class="iconochive-facebook" style="color:#3b5998;font-size:160%;"></span></a>
-	<a href="https://web.archive.org/web/20050428001304/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm#" onclick="window.open(&#39;https://twitter.com/intent/tweet?text=https://web.archive.org/web/20050428001304/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm&amp;via=internetarchive&#39;, &#39;&#39;, &#39;height=400,width=600&#39;); return false;" title="Share on Twitter" style="margin-right:5px;" target="_blank"><span class="iconochive-twitter" style="color:#1dcaff;font-size:160%;"></span></a>
-      </div>
-    </div>
-    <table class="c" style="">
-      <tbody>
-	<tr>
-	  <td class="u" colspan="2">
-	    <form target="_top" method="get" action="https://web.archive.org/web/submit" name="wmtb" id="wmtb"><input type="text" name="url" id="wmtbURL" value="http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm" onfocus="this.focus();this.select();" autocomplete="off"><input type="hidden" name="type" value="replay"><input type="hidden" name="date" value="20050428001304"><input type="submit" value="Go"></form>
-	  </td>
-	  <td class="n" rowspan="2" style="width:110px;">
-	    <table>
-	      <tbody>
-		<!-- NEXT/PREV MONTH NAV AND MONTH INDICATOR -->
-		<tr class="m">
-		  <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/20050219161148/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm" title="19 Feb 2005"><strong>Feb</strong></a></td>
-		  <td class="c" id="displayMonthEl" title="You are here: 00:13:04 Apr 28, 2005">APR</td>
-		  <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20070226232835/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm" title="26 Feb 2007"><strong>Feb</strong></a></td>
-		</tr>
-		<!-- NEXT/PREV CAPTURE NAV AND DAY OF MONTH INDICATOR -->
-		<tr class="d">
-		  <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/20050219161148/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm" title="16:11:48 Feb 19, 2005"><img src="./boot_files/wm_tb_prv_on.png" alt="Previous capture" width="14" height="16" border="0"></a></td>
-		  <td class="c" id="displayDayEl" style="width:34px;font-size:24px;white-space:nowrap;" title="You are here: 00:13:04 Apr 28, 2005">28</td>
-		  <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20070226232835/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm" title="23:28:35 Feb 26, 2007"><img src="./boot_files/wm_tb_nxt_on.png" alt="Next capture" width="14" height="16" border="0"></a></td>
-		</tr>
-		<!-- NEXT/PREV YEAR NAV AND YEAR INDICATOR -->
-		<tr class="y">
-		  <td class="b" nowrap="nowrap">2004</td>
-		  <td class="c" id="displayYearEl" title="You are here: 00:13:04 Apr 28, 2005">2005</td>
-		  <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20070226232835/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm" title="26 Feb 2007"><strong>2007</strong></a></td>
-		</tr>
-	      </tbody>
-	    </table>
-	  </td>
-	</tr>
-	<tr>
-	  <td class="s">
-	    	    <div id="wm-nav-captures"><a class="t" href="https://web.archive.org/web/*/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm" title="See a list of every capture for this URL">7 captures</a><div class="r" title="Timespan for captures of this URL">22 Jul 2004 - 26 Feb 2007</div></div>
-	  </td>
-	  <td class="k">
-	    <a href="https://web.archive.org/web/20050428001304/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm" id="wm-graph-anchor">
-	      <div id="wm-ipp-sparkline" title="Explore captures for this URL" style="position: relative">
-		<canvas id="wm-sparkline-canvas" width="575" height="27" border="0"></canvas>
-	      <div class="yt" style="display: none; width: 25px; height: 27px;"></div><div class="mt" style="display: none; width: 2px; height: 27px;"></div></div>
-	    </a>
-	  </td>
-	</tr>
-      </tbody>
-    </table>
-    <div style="position:absolute;bottom:0;right:2px;text-align:right;">
-      <a id="wm-expand" class="wm-btn wm-closed" href="https://web.archive.org/web/20050428001304/http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm#expand" onclick="__wm.ex(event);return false;"><span id="wm-expand-icon" class="iconochive-down-solid"></span> <span style="font-size:80%">About this capture</span></a>
-    </div>
-  </div>
-    <div id="wm-capinfo" style="border-top:1px solid #777;display:none; overflow: hidden">
-            <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center">COLLECTED BY</div>
-    <div style="padding:3px;position:relative" id="wm-collected-by-content">
-            <div style="display:inline-block;vertical-align:top;width:50%;">
-			<span class="c-logo" style="background-image:url(https://archive.org/services/img/alexacrawls);"></span>
-		Organization: <a style="color:#33f;" href="https://archive.org/details/alexacrawls" target="_new"><span class="wm-title">Alexa Crawls</span></a>
-		<div style="max-height:75px;overflow:hidden;position:relative;">
-	  <div style="position:absolute;top:0;left:0;width:100%;height:75px;background:linear-gradient(to bottom,rgba(255,255,255,0) 0%,rgba(255,255,255,0) 90%,rgba(255,255,255,255) 100%);"></div>
-	  Starting in 1996, <a href="http://www.alexa.com/">Alexa Internet</a> has been donating their crawl data to the Internet Archive.  Flowing in every day, these data are added to the <a href="http://web.archive.org/">Wayback Machine</a> after an embargo period.
-	</div>
-	      </div>
-      <div style="display:inline-block;vertical-align:top;width:49%;">
-			<span class="c-logo" style="background-image:url(https://archive.org/services/img/alexa_ed)"></span>
-		<div>Collection: <a style="color:#33f;" href="https://archive.org/details/alexa_ed" target="_new"><span class="wm-title">alexa_ed</span></a></div>
-		<div style="max-height:75px;overflow:hidden;position:relative;">
-	  <div style="position:absolute;top:0;left:0;width:100%;height:75px;background:linear-gradient(to bottom,rgba(255,255,255,0) 0%,rgba(255,255,255,0) 90%,rgba(255,255,255,255) 100%);"></div>
-	  this data is currently not publicly accessible.
-	</div>
-	      </div>
-    </div>
-    <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center" title="Timestamps for the elements of this page">TIMESTAMPS</div>
-    <div>
-      <div id="wm-capresources" style="margin:0 5px 5px 5px;max-height:250px;overflow-y:scroll !important"></div>
-      <div id="wm-capresources-loading" style="text-align:left;margin:0 20px 5px 5px;display:none"><img src="./boot_files/loading.gif" alt="loading"></div>
-    </div>
-  </div></div></div></div>
-<!-- BEGIN WAYBACK TOOLBAR INSERT -->
-<script type="text/javascript" src="./boot_files/timestamp.js" charset="utf-8"></script>
-<script type="text/javascript" src="./boot_files/graph-calc.js" charset="utf-8"></script>
-<script type="text/javascript" src="./boot_files/auto-complete.js" charset="utf-8"></script>
-<script type="text/javascript" src="./boot_files/toolbar.js" charset="utf-8"></script>
-<style type="text/css">
-body {
-  margin-top:0 !important;
-  padding-top:0 !important;
-  /*min-width:800px !important;*/
-}
-.wb-autocomplete-suggestions {
-    text-align: left; cursor: default; border: 1px solid #ccc; border-top: 0; background: #fff; box-shadow: -1px 1px 3px rgba(0,0,0,.1);
-    position: absolute; display: none; z-index: 2147483647; max-height: 254px; overflow: hidden; overflow-y: auto; box-sizing: border-box;
-}
-.wb-autocomplete-suggestion { position: relative; padding: 0 .6em; line-height: 23px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; font-size: 1.02em; color: #333; }
-.wb-autocomplete-suggestion b { font-weight: bold; }
-.wb-autocomplete-suggestion.selected { background: #f0f0f0; }
-</style>
-<script type="text/javascript">
-__wm.bt(575,27,25,2,"web","http://www.tg.rim.or.jp:80/~kiyoshi/source/boot.asm","2005-04-28",1996);
-</script><div class="wb-autocomplete-suggestions " style="left: 163px; top: 23px; width: 646px;"></div>
-<!-- END WAYBACK TOOLBAR INSERT --><iframe id="playback" src="./boot_files/boot.html" frameborder="0" style="position: absolute; top: 65px; left: 0px; width: 100%; height: 867px;">
-</iframe>
-<script type="text/javascript">
-var margin_top = 65;
-function fitPlayback() {
-  $('#playback').css({
-    height: (window.innerHeight - margin_top) + 'px',
-    top: margin_top
-  });
-}
-fitPlayback();
-$(window).on('resize', function() {
-  fitPlayback(); 
-});
-$('#wm-tb-close').click(function(){
-  margin_top = 0;
-  fitPlayback();
-});
-</script>
+;
+; boot.asm
+;
+; Most of the codes are taken from Linux 0.0.1 version
+; 
 
-</body></html>
+SECTORS	EQU 18		; number of sectors per track
+SYSSEG	EQU 0x1000	; segment where to load 32 bit program
+ENDSEG	EQU 0x9000	; end segment of 32 bit program
+
+; BIOS loads bootsector at linear address 0x7c00
+
+	ORG 0x7c00
+
+	SECTION .text
+
+	BITS 16
+
+start:
+	; save boot drive		;DS = 0x7c00
+	mov [bootdrv],dl
+
+	; change cursor
+	mov	ah,0x3   		; get cursor info
+	mov	bh,0			; page 0
+	int	0x10			; call bios
+	mov	cl,0x1			; starting scan line = 1
+	mov	ah,0x1			; set cursor info
+	int	0x10			; call bios
+
+	; print message
+	mov	si,message
+	call	print_string
+
+	; load 32 bit program
+	call   read_program
+	call   kill_motor
+
+	; clear interrupts
+	cli
+
+	; set up IDT & GDT
+	lidt [idt_info]
+	lgdt [gdt_info]
+
+	; enable A20
+	call	empty_8042
+	mov	al,0xd1		; command write
+	out	0x64,al
+	call	empty_8042
+	mov	al,0xdf		; A20 on
+	out	0x60,al
+	call	empty_8042
+
+	; enable protected mode
+	mov	ax,0x0001		; protected mode (PE) bit
+	lmsw	ax			; This is it!
+
+	; jump to 32 bit code
+	jmp	dword 0x8:kernel_32	; offset to kernel_32 function,  (0x8 = code segment)
+
+;
+; Empty 8042 function
+;
+; This routine checks that the keyboard command queue is empty
+; No timeout is used - if this hangs there is something wrong with
+; the machine, and we probably couldn't proceed anyway.
+;
+
+empty_8042:
+	in	al,0x64		; 8042 status port
+	test	al,2		; is input buffer full?
+	jnz	empty_8042	; yes - loop
+	ret
+
+;
+; Print string
+;
+
+print_string:
+	mov	ah,0xe		; bios function index
+	mov	bh,0		; console page 0
+.next:	mov	al,[si]		; move char into al
+	cmp	al,0		; is char == 0?
+	jz	.done		; yes, exit
+	int	0x10		; call bios
+	inc	si		; increment char pointer
+	jmp	.next		; next char
+.done:	ret
+
+;
+; Print sign
+;
+
+print_sign:
+	push 	bx
+	mov	ah,0xe
+	mov	al,'#'
+	int	0x10
+	pop	bx
+	ret
+;
+; Read program function 
+;
+; This routine loads the system at linear address 0x10000, making sure
+; no 64kB boundaries are crossed. We try to load it as fast as
+; possible, loading whole tracks whenever we can.
+;
+; This routine has to be recompiled to fit another drive type,
+; just change the "sectors" variable at the start of the file
+; (originally 18, for a 1.44Mb drive)
+;
+
+; SECTORS	EQU 18		; number of sectors per track
+; SYSSEG	EQU 0x1000	; segment where to load 32 bit program
+; ENDSEG	EQU 0x9000	; end segment of 32 bit program
+	
+read_program:
+				
+	mov ax,SYSSEG		; es is starting segment
+	mov es,ax		;  to load the program read from floppy
+	xor bx,bx		; bx is starting address within segment
+				; ax = 0x1000, es = 0x1000, bx=0
+rp_read:
+	call print_sign
+	mov ax,es		;  ax = es;
+	cmp ax,ENDSEG		; have we loaded all yet? (ax == 0x9000)
+	jb ok1_read		; Jump if below (<) ( carry = 1)
+	ret
+ok1_read:
+	mov ax,SECTORS		; ax = 18 (first time)
+	sub ax,[sread]		; 18 - 1 
+	mov cx,ax		; cx = 17
+	shl cx,9		; ? 
+	add cx,bx		; ?
+	jnc ok2_read		; Jump if no carry (carry = 0)
+	je ok2_read		; Jump if equal (=) (Zero = 1) 
+	xor ax,ax               ; ax = 0
+	sub ax,bx               ; ?
+	shr ax,9                ; ?
+ok2_read:
+	call read_track
+	mov cx,ax
+	add ax,[sread]
+	cmp ax,SECTORS
+	jne ok3_read		; Jump if not equal () (Zero = 0)
+	mov ax,1
+	sub ax,[head]
+	jne ok4_read
+	inc word [track]
+ok4_read:
+	mov [head],ax
+	xor ax,ax
+ok3_read:
+	mov [sread],ax
+	shl cx,9
+	add bx,cx
+	jnc rp_read
+	mov ax,es
+	add ax,0x1000
+	mov es,ax
+	xor bx,bx
+	jmp rp_read
+
+;bootdrv:	DW	1	; drive booted from
+;sread:		DW	1	; sectors read of current track
+;head:		DW	0	; current head
+;track:		DW	0	; current track
+	
+read_track:
+	push ax
+	push bx
+	push cx
+	push dx
+	
+	mov dx,[track]		; dl = 0
+	mov cx,[sread]		; cl = sread
+	inc cx			; cl = sread + 1;
+	mov ch,dl		; ch = 0;
+	
+	mov dx,[head]		; dx = 0;
+	mov dh,dl		; dh = 0;
+	
+	mov dl,0		; dl = 0
+	and dx,0x0100		; dh = 1
+	
+	mov ah,2
+	int 0x13
+	jc bad_rt
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	ret
+bad_rt:	mov ax,0		; reset the Floppy Drive
+	mov dx,0		;  dl = 0
+	int 0x13
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	jmp read_track
+
+kill_motor:
+	push dx
+	mov dx,0x3f2
+	mov al,0
+	outb
+	pop dx
+	ret	
+
+
+;
+; 32 bit function
+;
+
+		BITS 32
+
+kernel_32:	
+		; set segment registers
+		mov eax,0x10	; code segment register is already loaded, now load
+		mov ds,eax	; all the data segment registers with GDT descriptor 0x10
+		mov es,eax	; this finishes the flat memory model setup
+		mov fs,eax
+		mov gs,eax
+		mov ss,eax
+		
+		; disable caching
+		mov eax,cr0
+		or eax,0x60000000	; cache disable | not write through
+		mov cr0,eax
+		wbinvd				; flush cache
+
+		; count memory
+		mov eax,0x100000		; start at 1 mb mark
+		mov ebx,0x400			; 1 kb increments
+		mov ecx,0x12345678		; random bit pattern
+.next:		mov edx,[eax]			; save what was there
+		mov [eax],ecx			; write bit pattern
+		cmp [eax],ecx			; compare with bit pattern
+		jne .done			; exit if pattern was lost
+		mov [eax],edx			; restore what was there
+		add eax,ebx			; increment pointer
+		jmp .next
+.done:		mov esp,eax			; set stack pointer
+
+
+		; enable caching
+		mov eax,cr0
+		and eax,0x9fffffff	; !(cache disable | not write through)
+		mov cr0,eax
+	    
+
+		; jump to loaded program
+		jmp 0x10000			
+	       
+
+
+; Data
+
+	SECTION .data
+gdt:
+		; descriptor 0x0 (dummy segment)
+		DW	0,0,0,0
+
+		; descriptor 0x8 (code segment 0-FFFFFFFF)
+		DW	0xffff		; limit 15-0
+		DW	0x0000		; base 15-0
+		DW	0x9a00		; 9a = code read/exec, 00 = base 23-16 
+		DW	0x00cf		; 00 = base 31-24, c = 4096/386, f = limit 19-16
+
+		; descriptor 0x10 (data segment 0-FFFFFFFF)
+		DW	0xffff		; limit 15-0
+		DW	0x0000		; base 15-0
+		DW	0x9200		; 92 = data read/write, 00 = base 23-16
+		DW	0x00cf		; 00 = base 31-24, c = 4096/386, f = limit 19-16
+
+gdt_info:
+		DW	0x18		; gdt limit in bytes, 3 gdt entries
+		DW	gdt,0x0		; gdt base address low, high
+
+idt_info:
+		DW	0		; limit
+		DW	0, 0		; base
+		
+bootdrv:	DW	1	; drive booted from
+sread:		DW	1	; sectors read of current track
+head:		DW	0	; current head
+track:		DW	0	; current track
+
+message:	DB	'Loading ',0
+
+; bootsector must be 512 bytes long, adjust this constant to vary the length
+; of the bootsector section of the os image
+
+times 121 DB 0
+
+; times   510 - ($-$$) db 0
+
+; bootsector signature
+
+DW 0xAA55
